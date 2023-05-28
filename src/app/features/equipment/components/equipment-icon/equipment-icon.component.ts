857f5dba6e66b9973f1020e40b03df9303dc30b8
awndrooo <nomadigin@gmail.com>
2023-05-28 16:12:53 -0600

    Added rarity color for equipment icons.

--- src/app/features/equipment/components/equipment-icon/equipment-icon.component.ts
+++ src/app/features/equipment/components/equipment-icon/equipment-icon.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
 import { ArmorTypeEnum } from '@app/core/types/ArmorType';
 import { EQUIPMENT_MAPS } from '@app/core/types/EquipmentMap';
 import {
@@ -22,6 +27,10 @@ export class EquipmentIconComponent {
     | WeaponTypeEnum
     | undefined;
 
+  @HostBinding('class') get _hostClass() {
+    return this._getRarityClass();
+  }
+
   public get SvgIcon(): string {
     if (this.equipment != undefined) {
       if (typeof this.equipment !== 'string') {
@@ -36,5 +45,12 @@ export class EquipmentIconComponent {
       }
     }
     return '';
+  }
+
+  private _getRarityClass(): string | undefined {
+    if (this.equipment != undefined && typeof this.equipment !== 'string') {
+      return 'rarity-' + this.equipment.rarity;
+    }
+    return undefined;
   }
 }
