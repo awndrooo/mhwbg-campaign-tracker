82f97b85f2ef5dfa7a9d665d08cc747ffcec525e
awndrooo <nomadigin@gmail.com>
2023-05-28 19:22:42 -0600

    Fixed failed profile selector test.

--- src/app/features/campaign-editor/components/profile-selector/profile-selector.component.spec.ts
+++ src/app/features/campaign-editor/components/profile-selector/profile-selector.component.spec.ts
@@ -7,17 +7,26 @@ import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
+import { Subject } from 'rxjs';
 import { ProfileSelectorComponent } from './profile-selector.component';
 
 describe('ProfileSelectorComponent', () => {
   let component: ProfileSelectorComponent;
   let fixture: ComponentFixture<ProfileSelectorComponent>;
+  let actions$: Subject<Action>;
 
   beforeEach(async () => {
+    actions$ = new Subject<Action>();
+
     await TestBed.configureTestingModule({
       declarations: [ProfileSelectorComponent, MatIcon],
-      providers: [provideMockStore()],
+      providers: [
+        provideMockStore(),
+        provideMockActions(() => actions$.asObservable()),
+      ],
       imports: [
         MatDialogModule,
         MatFormFieldModule,
