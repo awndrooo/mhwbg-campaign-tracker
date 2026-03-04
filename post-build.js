a1b55a80c15093e65f4aa5357ede1ed3f82e0bc6
awndrooo <nomadigin@gmail.com>
2026-03-03 19:11:00 -0700

    Fixed post-build script

--- post-build.js
+++ post-build.js
@@ -3,21 +3,21 @@ import * as fs from 'node:fs/promises';
 // Render db out into separate files for each entity type for prod
 
 let file = await fs.readFile(
-  './dist/mhwbgcampaign-tracker/assets/db.json',
+  './dist/mhwbgcampaign-tracker/browser/assets/db.json',
   'utf-8'
 );
 
 let db = JSON.parse(file);
 
-const { materials, equipment } = db;
+const { materials, equipment, monsters } = db;
 
 await fs.writeFile(
-  './dist/mhwbgcampaign-tracker/assets/materials',
+  './dist/mhwbgcampaign-tracker/browser/assets/materials',
   JSON.stringify(materials),
   'utf-8'
 );
 await fs.writeFile(
-  './dist/mhwbgcampaign-tracker/assets/equipment',
+  './dist/mhwbgcampaign-tracker/browser/assets/equipment',
   JSON.stringify(equipment),
   'utf-8'
 );
